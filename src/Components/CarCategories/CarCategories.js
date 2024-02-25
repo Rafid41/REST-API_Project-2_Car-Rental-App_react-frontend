1f488eae811f3d441755d650f163cfb41727140f
Rafid41 <wap.alnahiyan525@gmail.com>
2024-02-25 21:15:02 +0600

    car search done

--- src/Components/CarCategories/CarCategories.js
+++ src/Components/CarCategories/CarCategories.js
@@ -2,47 +2,54 @@
 import React from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 import { Link } from "react-router-dom";
+import Search from "./Search";
 
 const CarCategories = (props) => {
     return (
-        <div className="container">
-            <h2
-                style={{
-                    textAlign: "center",
-                    fontWeight: "bold",
-                    marginBottom: "15px",
-                }}
-            >
-                Car Categories
-            </h2>
-            <ListGroup>
-                {props.categories.map((category, index) => {
-                    // <p>{category.category_name}</p>
-                    // convert object to string so that it can be passed by Link
-                    const categoryString = encodeURIComponent(
-                        JSON.stringify(category)
-                    );
-                    return (
-                        <Link
-                            to={`/car_list/${categoryString}`}
-                            key={index} //optional
-                            style={{ textDecoration: "None" }}
-                        >
-                            <ListGroupItem key={index} action>
-                                <p
-                                    style={{
-                                        fontSize: "20px",
-                                        padding: "5px",
-                                        fontWeight: "bold",
-                                    }}
-                                >
-                                    {category.category_name}
-                                </p>
-                            </ListGroupItem>
-                        </Link>
-                    );
-                })}
-            </ListGroup>
+        <div
+            className="container"
+            style={{ display: "flex", flexDirection: "row" }}
+        >
+            <div style={{ flex: "70%" }}>
+                <h2
+                    style={{
+                        textAlign: "center",
+                        fontWeight: "bold",
+                        marginBottom: "15px",
+                    }}
+                >
+                    Car Categories
+                </h2>
+                <ListGroup>
+                    {props.categories.map((category, index) => {
+                        const categoryString = encodeURIComponent(
+                            JSON.stringify(category)
+                        );
+                        return (
+                            <Link
+                                to={`/car_list/${categoryString}`}
+                                key={index}
+                                style={{ textDecoration: "None" }}
+                            >
+                                <ListGroupItem key={index} action>
+                                    <p
+                                        style={{
+                                            fontSize: "20px",
+                                            padding: "5px",
+                                            fontWeight: "bold",
+                                        }}
+                                    >
+                                        {category.category_name}
+                                    </p>
+                                </ListGroupItem>
+                            </Link>
+                        );
+                    })}
+                </ListGroup>
+            </div>
+            <div style={{ flex: "30%" }}>
+                <Search />
+            </div>
         </div>
     );
 };
---
+++ src/Components/CarCategories/Search.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+
+const mapStateToProps = (state) => {
+    return {
+        cars: state.cars,
+        categories: state.categories,
+    };
+};
+
+const Search = (props) => {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleInputChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    // Function to perform the search
+    const performSearch = () => {
+        if (searchQuery.length > 0) {
+            const filteredCars = props.cars.filter((car) =>
+                car.car_name.toLowerCase().includes(searchQuery.toLowerCase())
+            );
+            setSearchResults(filteredCars);
+        } else {
+            setSearchResults([]); // Set search results to an empty array when the search query is empty
+        }
+    };
+
+    return (
+        <div style={{ marginLeft: "2rem" }}>
+            <center>
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                    }}
+                >
+                    <input
+                        type="text"
+                        value={searchQuery}
+                        onChange={handleInputChange}
+                        placeholder="Search with Car Name..."
+                        className="form-control"
+                    />
+
+                    <button className="btn btn-success" onClick={performSearch}>
+                        Search
+                    </button>
+                </div>
+
+                {/* Display search results */}
+                <ListGroup>
+                    {searchResults.length > 0 && // Check if searchResults is not empty before rendering
+                        searchResults.map((car, index) => {
+                            const category = props.categories.find(
+                                (category) => category.id === car.car_category
+                            );
+                            if (category) {
+                                const carString = encodeURIComponent(
+                                    JSON.stringify(car)
+                                );
+                                const categoryString = encodeURIComponent(
+                                    JSON.stringify(category)
+                                );
+
+                                return (
+                                    <Link
+                                        to={`/car_detail/${carString}/${categoryString}`}
+                                        key={index}
+                                        style={{ textDecoration: "None" }}
+                                    >
+                                        <ListGroupItem key={index} action>
+                                            <p style={{ fontWeight: "bold" }}>
+                                                {car.car_name}
+                                            </p>
+                                        </ListGroupItem>
+                                    </Link>
+                                );
+                            } else {
+                                return null;
+                            }
+                        })}
+                </ListGroup>
+            </center>
+            <br />
+            <br />
+        </div>
+    );
+};
+
+export default connect(mapStateToProps)(Search);
